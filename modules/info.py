6ffdd108e8de846969112b58919d3a4dce84c568
DiNitride <j.ptb.t.ca@gmail.com>
2018-09-06 19:59:06 +0100

    Added information to access the source and contributing

--- modules/info.py
+++ modules/info.py
@@ -15,5 +15,25 @@ class Info(BaseCog):
         """
         await ctx.send("http://www.aber.ac.uk/en/")
 
+    @commands.command()
+    async def source(self, ctx):
+        """
+        Source code for the bot
+        """
+        await ctx.send("http://www.github.com/DiNitride/AberBot\n"
+                       f"If you want to contribute do {ctx.prefix}contributing")
+
+    @commands.command()
+    async def contributing(self, ctx):
+        """
+        Information about contributing
+        """
+        await ctx.send("If you wish to help contribute to the bot, then fork the repo on Github.\n"
+                       "The bot is written in Python, using an awfully named framework called Dinnerplate, "
+                       "which uses the discord.py (Rewrite) library to communicate with library.\n"
+                       "If don't know any Python then I would reccomend practicing with some small projects of your "
+                       "own first, but once you have the basics feel free to make yourself a module or "
+                       "contribute to an existing one.")
+
 
 setup = Info.setup
